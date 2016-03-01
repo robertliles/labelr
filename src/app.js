7ec9635a300f9adac021b70cd7865bee269abdf3
Robert Liles <robert.l.liles@gmail.com>
2016-02-29 20:21:31 -0500

    wip

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-document.write('Hello Frontend Masters!')
+var React = require('react')
 
+var Hello = React.createClass({
+    render: function() {
+        return <div>Hello, {this.props.name}</div>
+    }
+})
+
+React.render(<Hello name="bob"/>, document.body)
