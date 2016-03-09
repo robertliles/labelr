f0fe64c2f7552a3d7bf49aa57252cfe70f246290
Robert Liles <robert.l.liles@gmail.com>
2016-03-09 17:42:54 -0500

    Wed Mar  9 17:42:54 EST 2016

--- src/router.js
+++ src/router.js
@@ -1,17 +1,34 @@
-import Router from 'ampersand-router';
 import styles from './styles/main.styl';
 
+import React from 'react';
+import Router from 'ampersand-router';
+import PublicPage from './pages/public';
+import Layout from './layout';
+import ReposPage from './pages/repos';
+
 export default Router.extend({
+    renderPage(page, opts = {layout: true}) {
+        if (opts.layout) {
+            page = (
+                <Layout>
+                    {page}
+                </Layout> 
+            );
+        }
+
+        React.render(page, document.body);
+    },
+
     routes: {
         '': 'public',
         'repos': 'repos'
     },
 
     public() {
-        console.log('public');
+        this.renderPage(<PublicPage/>, {layout: false});
     },
 
     repos() {
-        console.log('repos');
+        this.renderPage(<ReposPage/>);
     }
 });
