3736b1b59a46cea35c33f5c5e479d754f6245170
Robert Liles <robert.l.liles@gmail.com>
2016-03-10 10:13:16 -0500

    wip

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import Router from './router';
+import app from 'ampersand-app';
+import Me from './models/me';
 
-window.app = {
+window.app = app;
+
+app.extend({
     init() {
+        this.me = new Me();
         this.router = new Router();
         this.router.history.start();
     }
-};
+});
 
-window.app.init();
+app.init();
--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
+import app from 'ampersand-app';
 import styles from './styles/main.styl';
-
+import qs from 'qs';
+import xhr from 'xhr';
 import React from 'react';
 import Router from 'ampersand-router';
 import PublicPage from './pages/public';
@@ -22,7 +24,10 @@ export default Router.extend({
     routes: {
         '': 'public',
         'public': 'public',
-        'repos': 'repos'
+        'login': 'login',
+        'logout': 'logout',
+        'repos': 'repos',
+        'auth/callback?:query': 'authCallback'
     },
 
     public() {
@@ -31,5 +36,31 @@ export default Router.extend({
 
     repos() {
         this.renderPage(<ReposPage/>);
+    },
+
+    // TODO: set up own
+    login() {
+        window.location = 'https://github.com/login/oauth/authorize?' + qs.stringify({
+            client_id: 'f8dd69187841cdd22a26',
+            redirect_uri: window.location.origin + '/auth/callback',
+            scope: 'user,repo'
+        });
+    },
+
+    authCallback(query) {
+        query = qs.parse(query);
+
+        xhr({
+            url: 'https://labelr-localhost.herokuapp.com/authenticate/' + query.code, // TODO: set up own
+            json: true 
+        }, (err, req, body) => {
+            app.me.token = body.token;
+            this.redirectTo('/repos');
+        });
+    },
+
+    logout() {
+        window.localStorage.clear();
+        window.location = '/';
     }
 });
