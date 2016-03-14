5400c68acb67ce02e971a7806cd801554662f3ee
Robert Liles <robert.l.liles@gmail.com>
2016-03-14 17:27:43 -0400

    render repos

--- src/pages/repos.js
+++ src/pages/repos.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import ampersandMixin from 'ampersand-react-mixin';
 
 export default React.createClass({
     render() {
+        const {repos} = this.props;
+
         return (
             <div>
                 <h2>Repos</h2>
+                <ul>
+                {repos.map((repo) =>
+                    <li key={repo.id}><a href="">{repo.full_name}</a></li>
+                )}
+                </ul>
             </div>
         );
     }
